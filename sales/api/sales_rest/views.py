668aed34c6067d149b5f86d4e734aaf7a5dd5c8d
phil sobrepena <JPLS791@gmail.com>
2023-07-25 11:40:51 -0700

    added backend poller, sales rest models, sales rest get post delete views, deleted broken unused form

--- sales/api/sales_rest/views.py
+++ sales/api/sales_rest/views.py
@@ -1,3 +1,121 @@
 from django.shortcuts import render
+from common.json import ModelEncoder
+from .models import SalesPerson, Customer, Sale
+import json
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
 
 # Create your views here.
+
+#listsalespeopleencoder
+class ListSalespeopleEncoder(ModelEncoder):
+    model = SalesPerson
+    properties = ["first_name", "last_name", "employee_id"]
+
+#listcustomersencoder
+class ListCustomersEncoder(ModelEncoder):
+    model = Customer
+    properties = ["first_name", "last_name", "address", "phone_number"]
+
+#listsalesencoder
+class ListSalesEncoder(ModelEncoder):
+    model = Sale
+    properties = ["automobile", "salesperson", "customer", "price"]
+
+
+#getpostdelete list salespeople
+@require_http_methods(["GET", "POST", "DELETE"])
+def api_list_salespeople(request, id):
+    if request.method == "GET":
+        salespeople = SalesPerson.objects.all()
+        return JsonResponse(
+            {"salespeople": salespeople},
+            encopder = ListSalespeopleEncoder,
+        )
+    elif request.method == "POST":
+        content = json.loads(request.body)
+        salesperson = SalesPerson.objects.create(**content)
+        return JsonResponse(
+            salesperson,
+            encoder=ListSalespeopleEncoder,
+            safe=False,
+        )
+    else: #DELETE
+        try:
+            salesperson = SalesPerson.objects.get(employee_id=id)
+            salesperson.delete()
+            return JsonResponse(
+                salesperson,
+                encoder=ListSalespeopleEncoder,
+                safe=False,
+            )
+        except SalesPerson.DoesNotExist:
+            return JsonResponse(
+                {"message": "salesperson does not exist"}
+            )
+
+
+#getpostdelete list customers
+@require_http_methods(["GET", "POST", "DELETE"])
+def api_list_customers(request, id):
+    if request.method == "GET":
+        customers = Customer.objects.all()
+        return JsonResponse(
+            {"customers": customers},
+            encoder=ListCustomersEncoder,
+        )
+    elif request.method == "POST":
+        content = json.loads(request.body)
+        customer = Customer.objects.create(**content)
+        return JsonResponse(
+            customer,
+            encoder=ListCustomersEncoder,
+            safe=False,
+        )
+    else:  # DELETE
+        try:
+            customer = Customer.objects.get(id=id)
+            customer.delete()
+            return JsonResponse(
+                customer,
+                encoder=ListCustomersEncoder,
+                safe=False,
+            )
+        except Customer.DoesNotExist:
+            return JsonResponse(
+                {"message": "Customer does not exist"}
+            )
+
+
+
+
+#getpostdelete list sales
+@require_http_methods(["GET", "POST", "DELETE"])
+def api_list_sales(request, id):
+    if request.method == "GET":
+        sales = Sale.objects.all()
+        return JsonResponse(
+            {"sales": sales},
+            encoder=ListSalesEncoder,
+        )
+    elif request.method == "POST":
+        content = json.loads(request.body)
+        sale = Sale.objects.create(**content)
+        return JsonResponse(
+            sale,
+            encoder=ListSalesEncoder,
+            safe=False,
+        )
+    else:  # DELETE
+        try:
+            sale = Sale.objects.get(id=id)
+            sale.delete()
+            return JsonResponse(
+                sale,
+                encoder=ListSalesEncoder,
+                safe=False,
+            )
+        except Sale.DoesNotExist:
+            return JsonResponse(
+                {"message": "Sale does not exist"}
+            )
--- sales/poll/poller.py
+++ sales/poll/poller.py
@@ -11,19 +11,28 @@ django.setup()
 
 # Import models from sales_rest, here.
 # from sales_rest.models import Something
+from sales_rest.models import AutomobileVO
 
 
 def poll(repeat=True):
     while True:
         print('Sales poller polling for data')
         try:
-            # Write your polling logic, here
-            # Do not copy entire file
+            response = requests.get("http://project-beta-inventory-api-1:8000/api/automobiles/")
+            content = json.loads(response.content)
+            for automobile in content["autos"]:
+                AutomobileVO.object.update_or_create(
+                    import_href=automobile["import_href"],
+                    defaults={
+                        "vin", automobile["vin"],
+                        "sold", automobile["sold"],
+                    }
+                )
 
             pass
         except Exception as e:
             print(e, file=sys.stderr)
-        
+
         if (not repeat):
             break
 
