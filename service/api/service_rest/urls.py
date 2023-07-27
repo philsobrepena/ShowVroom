0b74cab226297001179f39fed8a38566f39e93b3
Hung <hungduyhoang@gmail.com>
2023-07-27 09:25:47 -0700

    more tweaking

--- service/api/service_rest/urls.py
+++ service/api/service_rest/urls.py
@@ -10,12 +10,12 @@ from .views import (api_list_appointments,
 
 urlpatterns = [
     path("technicians/", api_list_technicians, name="api_list_technicians"),
-    path("technicians/", api_list_technicians, name="api_create_technicians"),
+    # path("technicians/", api_list_technicians, name="api_create_technicians"),
     path("technicians/<int:id>/", api_show_technician, name="api_show_technician"),
     path("technicians/<id>/", api_show_technician, name="api_delete_technician"),
 
     path("appointments/", api_list_appointments, name="api_list_appointments"),
-    path("appointments/", api_list_appointments, name="api_create_appointment"),
+    # path("appointments/", api_list_appointments, name="api_create_appointment"),
     path("appointments/<int:id>/", api_show_appointment, name="api_show_appointments"),
     path("appointments/<id>/", api_show_appointment, name="api_delete_appointment"),
     path("appointments/<id>/cancel/", api_cancel_appointment, name="api_cancel_appointment"),
